c3e97d695da2394b9fdda99cbaf4c1022df0f9ea
al-ruhanyat <alruhanyat994@gmail.com>
2022-09-13 18:59:59 +0600

    First from here

---
+++ 05_pr-1.py
@@ -0,0 +1,11 @@
+def greatest(num1, num2, num3 ):
+    if (num1>num2):
+        greater=num1
+    else:
+        greater=num2
+    if (num3>greater):
+        greater= num3
+    
+    return greater
+a=greatest(7,8,92)
+print("The greatest number is : ",a)
