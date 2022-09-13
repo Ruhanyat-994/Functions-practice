9b9e661bfb700dc28fbf3b4cfd53854bf478cd2a
al-ruhanyat <alruhanyat994@gmail.com>
2022-09-13 20:07:34 +0600

    5th from here

---
+++ 09_pr-5.py
@@ -0,0 +1,4 @@
+def inches2centimetere(inch):
+    return  ( 2.54 * inch)
+a= inches2centimetere(45)
+print(a)
---
+++ 10_pr-6.py
@@ -0,0 +1,6 @@
+def multiplicationtable(n):
+    for i in range(1,n+1):
+        print(f"{n}X{i}={n*i}")
+    return n 
+a=multiplicationtable(10)
+print(a)
