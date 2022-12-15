e89647370777a4ffb592c596fbf5fb6d40bd23ce
Ruhanyat-994 <alruhanyat994@gmail.com>
2022-12-15 23:48:26 +0600

    Lambda function

---
+++ 11_pr-7.py
@@ -0,0 +1,13 @@
+# Lambda function
+
+# lambda function- anonymous function
+
+def sum(a,b):
+  
+  return a+b
+
+sum2 = lambda a,b : a*b
+print(sum2(4,5)) 
+
+greet = lambda greet,name : f"{greet} {name}"
+print(greet("hi","Ruhanyat"))
